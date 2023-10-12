c00b83e45079d9e1decc2a067dab2fd6631b982b
nguyengiabinh <115540912+nguyengiabinh@users.noreply.github.com>
2023-10-12 16:55:26 +0700

    in class code week 6

---
+++ Week 6/Student in_class/Student in_class/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Xml.Linq;
+
+namespace week6
+{
+    public class Program
+    {
+        static Dictionary<int, Student> StudentList = new Dictionary<int, Student>();
+        private static void Main(string[] args)
+        {
+          while (true) 
+            { 
+                Start_program();
+            }
+        }
+
+        public static void Start_program() 
+        {
+            while (true)
+            {
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Student();
+                        break;
+                    case 2:
+                        //StudentSearch();
+                        return;
+                }
+            }
+        }
+
+        public static void Student()
+        {
+            string name = Console.ReadLine();
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+            string address = Console.ReadLine();
+            Student student = new Student(name, birth, address);
+            student.ID = StudentList.Count + 1;
+            StudentList.Add(student.ID, student);
+            student.Display();
+        }
+    }
+}
---
+++ Week 6/Student in_class/Student in_class/Student.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week6
+{
+    public class Student
+    {
+        private int _id;
+        private string _name;
+        private DateTime _birth;
+        private string _address;
+
+        public Student (string name, DateTime birth, string address)
+        {
+            _name = name;
+            _birth = birth;
+            _address = address;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set 
+            {
+                _id = value;
+            } 
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public DateTime Birth
+        {
+            get
+            {
+                return _birth;
+            }
+            set
+            {
+                _birth = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return _address;
+            }
+            set
+            {
+                _address = value;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("StudentID : " + ID);
+            Console.WriteLine("Student Name: " + Name);
+            Console.WriteLine("Date of Birth: " + Birth.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Address : " + Address);
+        }
+
+
+    }
+}
+
+
