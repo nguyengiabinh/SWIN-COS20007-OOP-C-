9e08cd34e98caa90d64e335f032e138368e9dc1a
nguyengiabinh <115540912+nguyengiabinh@users.noreply.github.com>
2023-11-02 21:09:31 +0700

    in class code and a bit of custom code

---
+++ week 9/Student in_class/Student in_class/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Xml.Linq;
+using static week6.Student;
+
+namespace week6
+{
+    public class Program
+    {
+        static Dictionary<int, Student> StudentList = new Dictionary<int, Student>();
+        private static void Main(string[] args)
+        {
+          while (true) 
+            { 
+                Start_program();
+            }
+        }
+
+        public static void Start_program() 
+        {
+            while (true)
+            {
+                Console.WriteLine("pls enter a number choice: 1 or 2");
+                int choice = int.Parse(Console.ReadLine());
+                
+                switch (choice)
+                {
+                    case 1:
+                        InsertStudent();
+                        break;
+                    case 2:
+                        //StudentSearch();
+                        return;
+                }
+            }
+        }
+
+        public static void InsertStudent()
+        {
+            Console.WriteLine("pls enter firstname");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("pls enter lastname");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("pls enter gender");
+            string gender = Console.ReadLine().ToLower();
+            check_gender_input();
+            void check_gender_input()
+            {
+                if (gender != "male" && gender != "female")
+                {
+                    gender = "LGBTQ+";
+                }
+                else
+                {
+                    return;
+                }
+            }
+            Console.WriteLine("pls enter the student gpa");
+            string student_gpa = Console.ReadLine();
+            double gpa = double.Parse(student_gpa);
+            Console.WriteLine("pls enter one of the follow: freshman, sophomore, junior, senior");
+            string enter_status = Console.ReadLine().ToLower();
+            if (Enum.TryParse(enter_status, true, out status student_status))
+            {
+                Console.WriteLine("Insert complete");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. valid input are: freshman, sophomore, junior, senior");
+            }
+            Student student = new Student( firstname, lastname, gender, gpa, student_status);
+            student.show_myself();
+        }
+
+    }
+}
---
+++ week 9/Student in_class/Student in_class/Student.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week6
+{
+    public class Student
+    {
+        public enum status { freshman, sophomore, junior, senior}
+        private double gpa;
+        private string _firstname;
+        private string _lastname;
+        private string gender;
+        private status student_status;
+
+        public Student (string firstname, string lastname, string gender, double gpa, status student_status)
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+            this.gender = gender;
+            this.gpa = gpa;
+            this.student_status = student_status;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstname;
+            }
+            set 
+            {
+                _firstname = value;
+            } 
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                _lastname = value;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender = value;
+            }
+        }
+
+        public status Status
+        {
+            get
+            {
+                return student_status;
+            }
+            set
+            {
+                student_status = value;
+            }
+        }
+
+        public double GPA
+        {
+            get 
+            { 
+                return gpa;
+            }
+            set
+            { 
+                gpa = value; 
+            }
+        }
+
+        public void show_myself()
+        {
+            Console.WriteLine("Student Name: " + FirstName + " " + LastName);
+            Console.WriteLine("Gender: " + Gender);
+            Console.WriteLine("GPA : " + GPA);
+            Console.WriteLine("Status : " + Status);
+        }
+
+        //public study_time()
+        //{
+        //    if (gpa > 4.0d) 
+        //    {
+        //        double gpa = 4.0d;
+        //    }
+        //}
+
+
+    }
+}
+
+
